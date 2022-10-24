3db89f059515db9c2ccc5827a0ee7cc850265942
Matheus Herman <69770331+matheusherman@users.noreply.github.com>
2022-10-24 12:23:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var c=0;
+$("#change").click(function() {
+  if (c == 1) {
+  acerto();
+}
+
+  $("#change").html("<a onclick='acerto()'>Sim</a>");
+  $("#move").show();
+  c=1;
+});
+
+$("#move").hover(function() {
+  change();
+});
+
+function acerto() {
+  alert("Te Amo !");
+}
+
+function change()
+{
+    var i = Math.floor(Math.random()*300)+1;
+    var j = Math.floor(Math.random()*400)+1;
+    $("#move").css({top: j+'px',left: i+'px'});
+}
